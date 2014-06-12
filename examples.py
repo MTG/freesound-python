83bd7778b305ff4b734e64df23ba9489279e0b65
Gerard Roma <gerard.roma@upf.edu>
2014-06-12 10:06:22 +0200

    first APIv2 version

--- examples.py
+++ examples.py
@@ -1,82 +1,76 @@
-from freesound.__init__ import *
-Freesound.set_api_key('YOUR_API_KEY_HERE')
+import freesound
+c = freesound.FreesoundClient()
+c.set_token("36e286ba563287f86eed608c13cacd520ced010e","token")
 
 # Get sound info example
 print "Sound info:"
 print "-----------"
-s = Sound.get_sound(96541)
-print "Getting sound: " + s['original_filename']
-print "Url: " + s['url']
-print "Description: " + s['description']
-print "Tags: " + str(s['tags'])
+s = c.get_sound(96541)
+print "Getting sound: " + s.name
+print "Url: " + s.url
+print "Description: " + s.description
+print "Tags: " + str(s.tags)
 print "\n"
 
 # Get sound analysis example
 print "Get analysis:"
 print "-------------"
 analysis = s.get_analysis()
-mfcc = analysis['lowlevel']['mfcc']
+mfcc = analysis.lowlevel.mfcc.mean
 print "Mfccs: " + str(mfcc)
 print "\n"
 
 # Get similar sounds example
 print "Similar sounds: "
 print "---------------"
-similar_sounds = s.get_similar()
-for sound in similar_sounds['sounds']:
-    print "\t- " + sound['original_filename'] + " by " + sound['user']['username']
+results_pager = s.get_similar()
+for i in range(0, len(results_pager.results)):
+    sound = results_pager[i]
+    print "\t- " + sound.name + " by " + sound.username
 print "\n"
 
 # Search Example
 print "Searching for 'violoncello':"
 print "----------------------------"
-results = Sound.search(q="violoncello",filter="tag:tenuto duration:[1.0 TO 15.0]",sort="rating_desc")
-print "Num results: " + str(results['num_results'])
+results_pager = c.text_search(query="violoncello",filter="tag:tenuto duration:[1.0 TO 15.0]",sort="rating_desc")
+print "Num results: " + str(results_pager.count)
 print "\t ----- PAGE 1 -----"
-for sound in results['sounds']:
-    print "\t- " + sound['original_filename'] + " by " + sound['user']['username']
+for i in range(0, len(results_pager.results)):
+    sound = results_pager[i]
+    print "\t- " + sound.name + " by " + sound.username
+print "\t ----- PAGE 2 -----"
+results_pager = results_pager.next_page()
+for i in range(0, len(results_pager.results)):
+    sound = results_pager[i]
+    print "\t- " + sound.name + " by " + sound.username
 print "\n"
-
 
 # Content based search example
 print "Content based search:"
 print "---------------------"
-results = Sound.content_based_search(f=".lowlevel.pitch.var:[* TO 20]",
-    t='.lowlevel.pitch_salience.mean:1.0 .lowlevel.pitch.mean:440',
-    fields="preview-hq-ogg,duration,tags,url",
-    max_results="15",
-    sounds_per_page="10")
+results_pager = c.content_based_search(descriptors_filter="lowlevel.pitch.var:[* TO 20]",
+    target='lowlevel.pitch_salience.mean:1.0 lowlevel.pitch.mean:440')
 
-print "Num results: " + str(results['num_results'])
-for sound in results['sounds']:
-    print "\t- " + sound['url']
+print "Num results: " + str(results_pager.count)
+for i in range(0, len(results_pager.results)):
+    sound = results_pager[i]
+    print "\t- " + sound.name + " by " + sound.username
 print "\n"
-
-
-# Query for geotags example
-print "Geoquery example:"
-print "-----------------"
-results = Sound.geotag(min_lon=2.005176544189453,
-                       max_lon=2.334766387939453,
-                       min_lat=41.3265528618605,
-                       max_lat=41.4504467428547)
-print "Num results: " + str(results['num_results'])
-for sound in results['sounds']:
-    print "\t- " + sound['url']
-print "\n"
-
 
 
 # Getting sounds from a user example
 print "User sounds:"
 print "-----------"
-u = User.get_user("Jovica")
-print "User name: " + u['username']
-pager = u.sounds()
+u = c.get_user("Jovica")
+print "User name: " + u.username
+results_pager = u.get_sounds()
+print "Num results: " + str(results_pager.count)
 print "\t ----- PAGE 1 -----"
-for s in pager['sounds']:
-    print "\t- " + str(s['id'])
-pager.next()
+for i in range(0, len(results_pager.results)):
+    sound = results_pager[i]
+    print "\t- " + sound.name + " by " + sound.username
 print "\t ----- PAGE 2 -----"
-for s in pager['sounds']:
-    print "\t- " + str(s['id'])
+results_pager = results_pager.next_page()
+for i in range(0, len(results_pager.results)):
+    sound = results_pager[i]
+    print "\t- " + sound.name + " by " + sound.username
