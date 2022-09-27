e4f21b08ce0a2ea472f4ef5a963e756f316d2a70
Bomme <13520622+Bomme@users.noreply.github.com>
2022-09-27 22:31:21 +0200

    raise min required version of requests to 2.27 since we rely on JSONDecodeError

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(name='freesound-python',
       version='2.0',
       py_modules=['freesound'],
-      install_requires=['requests<3.0,>2.0'],
+      install_requires=['requests<3.0,>2.27'],
       python_requires='>=3.6'
       )
