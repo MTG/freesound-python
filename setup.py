0d38c99cff047478df45c36e4f3530020ebc5b02
Bomme <13520622+Bomme@users.noreply.github.com>
2022-10-02 23:55:43 +0200

    set version to 1.1

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
 
-setup(name='freesound-python',
-      version='2.0',
-      py_modules=['freesound'],
-      install_requires=['requests<3.0,>2.27'],
-      python_requires='>=3.6'
-      )
+setup(
+    name="freesound-python",
+    version="1.1",
+    py_modules=["freesound"],
+    install_requires=["requests<3.0,>2.27"],
+    python_requires=">=3.6",
+)
